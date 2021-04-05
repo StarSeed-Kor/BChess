9faa43387fa8991ce77839f20ee4f1089c45dfd1
yewon <wowuxo@gmail.com>
2021-04-05 20:50:55 +0900

    Main
    메인화면 기본 기능 추가

---
+++ BuffaloChess/Assets/Scripts/OnOff.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClickMenu(GameObject Menu)
+    {
+        if (Menu.activeSelf)
+        {
+            Menu.SetActive(false);
+        }
+        else
+        {
+            Menu.SetActive(true);
+        }
+    }
+}
