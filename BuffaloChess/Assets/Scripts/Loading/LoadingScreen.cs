725f16c8f78aa46bbfd1b7332c89c70a338e5715
yewon <wowuxo@gmail.com>
2021-04-28 12:37:46 +0900

    loading
    로딩화면 백그라운드와 텍스트 깜빡임 구현

---
+++ BuffaloChess/Assets/Scripts/Loading/LoadingScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LoadingScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //나중에 touch로 바꿔야함
+        if (Input.GetMouseButtonDown(0))
+        {
+                SceneManager.LoadScene("Main");
+        }
+    }
+}
---
+++ BuffaloChess/Assets/Scripts/Loading/TouchBlink.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchBlink : MonoBehaviour
+{
+    public Text TouchText;
+    float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time < 0.5)
+        {
+           TouchText.color = new Color(0.5f, 0.8f, 1, 1 - time);
+        }
+        else
+        {
+            TouchText.color = new Color(0.5f, 0.8f, 1, time);
+            if (time > 1f)
+            {
+                time = 0;
+            }
+        }
+        time += Time.deltaTime;
+    }
+
+}
