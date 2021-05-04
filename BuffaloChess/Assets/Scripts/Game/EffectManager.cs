898a9e913142ca59c75c2a540120946c3e63a7af
StarSeed-Kor <ahshrnak@gmail.com>
2021-05-04 19:44:36 +0900

    Fixed_Bug
    1. 자칼 이벤트로 이동했을때 해당 자리로 이동 불가했던 버그 수정

---
+++ BuffaloChess/Assets/Scripts/Game/EffectManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    public float Timer1;
+    public GameObject Event1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer1 -= Time.deltaTime;
+
+        if (Timer1 < 0 && this.name == Event1.name)
+        {
+            Event1.SetActive(false);
+        }
+        
+    }
+}
---
+++ BuffaloChess/Assets/Scripts/Game/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float Game_Timer;
+    float Max_Time = 3f;
+
+    public GameObject controller;
+    GameObject reference = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Game_Timer = Max_Time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Game_Timer -= Time.deltaTime;
+
+        if (Game_Timer < 0)
+        {
+            Debug.Log("Next Turn!");
+
+            controller = GameObject.FindGameObjectWithTag("GameController");
+
+            /*controller.GetComponent<Game>().SetPositionEmpty(reference.GetComponent<ChessManager>().GetXBoard(),
+            reference.GetComponent<ChessManager>().GetYBoard());*/
+
+            /*reference.GetComponent<ChessManager>().SetXBoard(matrixX);
+            reference.GetComponent<ChessManager>().SetYBoard(matrixY);
+            reference.GetComponent<ChessManager>().SetCoord();*/
+
+            //controller.GetComponent<Game>().SetPosition(reference);
+
+            //controller.GetComponent<Game>().NextTurn();
+
+            //reference.GetComponent<ChessManager>().DestroyMovePlates();
+
+            Game_Timer = Max_Time;
+        }
+    }
+}
