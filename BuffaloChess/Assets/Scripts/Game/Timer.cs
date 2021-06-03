1c12629448bec5480032d2430d8f10c4d6fb0d5d
StarSeed-Kor <ahshrnak@gmail.com>
2021-06-03 15:51:58 +0900

    Update&Fix
    1. 타이머 관련 애니메이션 받는 오브젝트 추가
    2. 레이어 추가 (Back, Middle, Fore)
    3. Diary 등 화면 조정

--- BuffaloChess/Assets/Scripts/Game/Timer.cs
+++ BuffaloChess/Assets/Scripts/Game/Timer.cs
@@ -5,13 +5,19 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    [HideInInspector]
     public float Game_Timer;
-    public float Max_Time = 3f;
+    public float Max_Time = 5f;
     public Text TurnText;
     public Text PlayerText;
 
     public GameObject controller;
-    GameObject reference = null;
+    /*GameObject reference = null;*/
+
+    public GameObject[] Enemy_Timer_Object = new GameObject[3];
+    public GameObject[] Player_Timer_Object = new GameObject[3];
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +32,41 @@ public class Timer : MonoBehaviour
 
         Timer_Update();
         Buffalo_Check();
+        Alert_Time();
+    }
+
+    void Alert_Time()
+    {
+        if(controller.GetComponent<Game>().GetCurrentPlayer() == "white")
+        {
+            if (Game_Timer <= 3f)
+            {
+                Player_Timer_Object[0].GetComponent<Animator>().Play("Timer_Up");
+            }
+            if (Game_Timer <= 2f)
+            {
+                Player_Timer_Object[1].GetComponent<Animator>().Play("Timer_Up");
+            }
+            if (Game_Timer <= 1f)
+            {
+                Player_Timer_Object[2].GetComponent<Animator>().Play("Timer_Up");
+            }
+        }
+        else if(controller.GetComponent<Game>().GetCurrentPlayer() == "black")
+        {
+            if ( Game_Timer <= 3f)
+            {
+                Enemy_Timer_Object[0].GetComponent<Animator>().Play("Timer_Up");
+            }
+            if (Game_Timer <= 2f)
+            {
+                Enemy_Timer_Object[1].GetComponent<Animator>().Play("Timer_Up");
+            }
+            if (Game_Timer <= 1f)
+            {
+                Enemy_Timer_Object[2].GetComponent<Animator>().Play("Timer_Up");
+            }
+        }
     }
 
     void Buffalo_Check()
@@ -43,6 +84,12 @@ public class Timer : MonoBehaviour
             }
 
             controller.GetComponent<Game>().NextTurn();
+
+            for(int i = 0; i < 3; i++)
+            {
+                Enemy_Timer_Object[i].GetComponent<Animator>().Play("Timer_Down");
+                Player_Timer_Object[i].GetComponent<Animator>().Play("Timer_Down");
+            }
 
             Game_Timer = Max_Time;
         }
